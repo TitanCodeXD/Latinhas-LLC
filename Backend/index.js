375297475d84c0c0367d411a7575c57a233a2f47
Wesley Santos <wesleysantos32892653@outlook.com>
2025-11-04 19:26:30 -0300

    Initial planning to CRUD in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,18 @@
 //To-do
+
+/* 1 - Criar/Cadastrar Demanda - POST */
+//To-do
+
+/* 2 - Listar todas demandas - GET */
+//To-do
+
+/* 3 - Editar demandas by ID - PUT */
+//To-do
+
+/* 4 - Deletar demanda by ID - Delete */
+//To-do
+
+/* Extras: Talvez adicionar, apenas para uma API mais 'robusta' e para facilitar nos meus teste 
+quando eu for checar nos insomnia, creio que ajude*/
+/* 5 - Buscar demanda by ID - GET */
+/* 6 - ? */
