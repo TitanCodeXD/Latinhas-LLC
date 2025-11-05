8b1cc33bf17beb57e39b239188bd1f8f57b7094c
Wesley Santos <wesleysantos32892653@outlook.com>
2025-11-04 23:38:24 -0300

    Initial Frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome Latinhas LLC</h1>
+            <Button variant="default">Get Started</Button>
+        </div>
+    );
+}
